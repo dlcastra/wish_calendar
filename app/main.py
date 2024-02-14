bda6128e38a8deb176d7b99620f2a0909b96192d
dlcastra <1dlcastra@gmail.com>
2024-02-14 17:10:40 +0100

    Added a minimum robotable data record to the database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import os
-import sqlite3
 from datetime import datetime
 
 import kivy
@@ -11,6 +10,7 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 
 from wishes import wish_list
+from helpers import to_db, from_db
 import random
 
 # Config
@@ -22,10 +22,11 @@ Config.set("graphics", "height", "600")
 class CalendarApp(App):
     def __init__(self):
         super().__init__()
+        now = datetime.now()
         self.date_label = None
         self.wish_label = None
         self.image = None
-        self.today = datetime.today()
+        self.today = now.date()
         self.wishes = [wish for wish in wish_list]
         self.paths = [
             "images/winter/",
@@ -34,10 +35,54 @@ class CalendarApp(App):
             "images/autumn/",
         ]
 
-    def build(self):  # Main logic
-        # conn = sqlite3.connect("app_data.db")
-        # c = conn.cursor()
+    def get_wish(self):  # Logic for creating wishes.
+        wish = random.choice(self.wishes)
+        return wish
 
+    def get_random_image(self, index: int):
+        path = self.paths[index]
+        get_images = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
+        choice_image = random.choice(get_images)
+        image = os.path.join(path, choice_image)
+
+        return image
+
+    def get_background_image(self):  # Logic of background selection depending on the time of year.
+        month = datetime.now().month
+        if month in [12, 1, 2]:
+            return self.get_random_image(0)
+        elif month in [3, 4, 5]:
+            return self.get_random_image(1)
+        elif month in [6, 7, 8]:
+            return self.get_random_image(2)
+        else:
+            return self.get_random_image(3)
+
+    def create_new_record(self):
+        today = datetime.today()
+        wish = self.get_wish()
+        background_image = self.get_background_image()
+        to_db(today, wish, background_image)
+
+    def update_time(self, _dt):  # Updating the date depending on the time of day
+        db_data = from_db()
+        date = db_data[1]
+        wish = db_data[2]
+        background_image = db_data[3]
+        if self.today == date:
+            self.date_label.text = f"{self.today.day} {self.today.strftime('%B')}"
+            self.wish_label.text = wish
+            self.image.source = background_image
+
+        else:
+            new_today = datetime.today()
+            if new_today.day != self.today.day:
+                self.today = new_today
+                self.date_label.text = f"{self.today.day} {self.today.strftime('%B')}"
+                self.wish_label.text = self.get_wish()
+                self.image.source = self.get_background_image()
+
+    def build(self):  # Main logic
         root = FloatLayout()
         self.image = Image(source=self.get_background_image(), size_hint=(1, 1))
 
@@ -64,45 +109,10 @@ class CalendarApp(App):
         root.add_widget(self.image)
         root.add_widget(self.date_label)
         root.add_widget(self.wish_label)
+        to_db(self.today, self.wish_label.text, self.image.source)
 
-        Clock.schedule_interval(self.update_time, 60)
+        Clock.schedule_interval(self.update_time, 10)
         return root
-
-    def get_wish(self):  # Logic for creating wishes.
-        hour = datetime.now().hour
-        if hour < 12:
-            return self.wishes[0]
-        elif hour < 18:
-            return self.wishes[1]
-        else:
-            return self.wishes[2]
-
-    def get_random_image(self, index: int):
-        path = self.paths[index]
-        get_images = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
-        choice_image = random.choice(get_images)
-        image = os.path.join(path, choice_image)
-
-        return image
-
-    def get_background_image(self):  # Logic of background selection depending on the time of year.
-        month = datetime.now().month
-        if month in [12, 1, 2]:
-            return self.get_random_image(0)
-        elif month in [3, 4, 5]:
-            return self.get_random_image(1)
-        elif month in [6, 7, 8]:
-            return self.get_random_image(2)
-        else:
-            return self.get_random_image(3)
-
-    def update_time(self, _dt):  # Updating the date depending on the time of day
-        new_today = datetime.today()
-        if new_today.day != self.today.day:
-            self.today = new_today
-            self.date_label.text = f"{self.today.day} {self.today.strftime('%B')}"
-            self.wish_label.text = self.get_wish()
-            self.image.source = self.get_background_image()
 
 
 if __name__ == "__main__":

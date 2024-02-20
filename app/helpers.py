5bbf330767e5c17be445e7c085cfe59c6c0d77df
dlcastra <1dlcastra@gmail.com>
2024-02-20 15:32:48 +0100

    Support functions for selecting wishes and checking if they are available in the database.

---
+++ app/helpers.py
@@ -0,0 +1,39 @@
+import os
+import random
+import sys
+import sqlite3
+
+from app.wishes import wish_list
+
+
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
+def get_wishes_from_db():
+    conn = sqlite3.connect("calendar.db")
+    with conn as cust_conn:
+        cursor = cust_conn.cursor()
+        cursor.execute(
+            """
+            SELECT wish FROM events
+            """
+        )
+        result = cursor.fetchall()
+
+    return result
+
+
+def generate_wish():
+    get_wish = [wish for wish in wish_list]
+    result = random.choice(get_wish)
+    return result
+
+
+def save_or_get_other(new_wish, wishes_from_db):
+    while new_wish in wishes_from_db:
+        new_wish = generate_wish()
+
+    return new_wish

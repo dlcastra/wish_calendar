f83b680840473c70d5c759ca367d190edeac5c66
dlcastra <1dlcastra@gmail.com>
2024-02-29 15:31:47 +0100

    Added a feature to shorten the photo path for backgrounds

--- app/helpers.py
+++ app/helpers.py
@@ -1,5 +1,6 @@
 import os
 import random
+import re
 import sys
 import sqlite3
 
@@ -37,3 +38,25 @@ def save_or_get_other(new_wish, wishes_from_db):
         new_wish = generate_wish()
 
     return new_wish
+
+
+def get_short_path(path):
+    pattern = re.compile(r"\bimages.*")
+    short_path = pattern.search(path)
+    math_path = short_path.group()
+
+    return math_path
+
+
+def db_is_full():
+    db_content = get_wishes_from_db()
+    if len(db_content) == 367:
+        conn = sqlite3.connect("calendar.db")
+        cursor = conn.cursor()
+        cursor.execute(
+            """DROP TABLE events"""
+        )
+
+
+if __name__ == "__main__":
+    print(len(get_wishes_from_db()))
